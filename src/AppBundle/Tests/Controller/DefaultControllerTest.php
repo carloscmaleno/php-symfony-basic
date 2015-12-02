7472f22c3686d2053de3d37ea297ed4bcd0d175a
Carlos Coronado <carlos.coronado@laviniainteractiva.com>
2015-12-02 16:55:29 +0100

    Add Test login

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,6 +4,7 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use UserBundle\Entity\User;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -31,5 +32,38 @@ class DefaultControllerTest extends WebTestCase
     {
         $this->client->request('GET', '/login');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLoginError()
+    {
+        $this->client->request('POST', '/login', array('email' => 'fake@email.com', 'password' => 'fakepassword'));
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('false', $this->client->getResponse()->getContent());
+        $this->assertContains('json', $this->client->getResponse()->headers->get('Content-Type'));
+    }
+
+    public function testLoginSuccess()
+    {
+        $encoder = static::$kernel->getContainer()->get('security.password_encoder');
+        $em = static::$kernel->getContainer()->get('doctrine')->getManager();
+
+        $user = new User();
+        $user->setUsername('phpunit');
+        $user->setEmail('php@unit.com');
+        $user->setRoles(array('ROLE_USER'));
+
+        $encodedPassword = $encoder->encodePassword($user, 'phpunit');
+        $user->setPassword($encodedPassword);
+
+        $em->persist($user);
+        $em->flush();
+
+        $this->client->request('POST', '/login', array('email' => 'php@unit.com', 'password' => 'phpunit'));
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('true', $this->client->getResponse()->getContent());
+        $this->assertContains('json', $this->client->getResponse()->headers->get('Content-Type'));
+
+        $em->remove($user);
+        $em->flush();
     }
 }
